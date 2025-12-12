dba08f7896a3af7a9abdd8562115c27112938e31
IT21279584 <it21279584@my.sliit.lk>
2025-12-12 22:19:49 +0530

    Initial Commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const app = require("./app");
-const connectDB = require("./config/database");
+const app = require("./src/app");
+const connectDB = require("./src/config/database");
 
 // Handle uncaught exceptions
 process.on("uncaughtException", (err) => {
