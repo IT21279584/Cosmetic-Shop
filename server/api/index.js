41df7f93d30d69a1bc86bd6c285cadf5c511cb6c
IT21279584 <it21279584@my.sliit.lk>
2025-12-12 23:28:08 +0530

    Initial Commit

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const app = require("../server/src/app");
-const connectDB = require("../server/src/config/database");
+const app = require("../src/app");
+const connectDB = require("../src/config/database");
 
 // Initialize database connection with caching
 let isConnected = false;
